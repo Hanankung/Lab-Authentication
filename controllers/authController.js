3f45728f5978f9b485e6fc8a1b9a37ae57392a3b
HananKung <hanannnn719@gmail.com>
2024-10-05 16:24:07 +0700

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,10 +5,10 @@ const User = require("../model/user"); // Corrected from Product to User
 
 // Register
 exports.register = async (req, res) => {
-    const { user_name, password ,name,role} = req.body;
+    const { user_name, password ,name,role,email} = req.body;
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ user_name, password: hashedPassword ,name,role });
+        const user = new User({ user_name, password: hashedPassword ,name,role,email });
         await user.save();
         res.status(201).send("User registered");
     } catch (err) {
@@ -18,9 +18,9 @@ exports.register = async (req, res) => {
 
 // Login
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
+    const { user_name, password } = req.body;
     try {
-        const user = await User.findOne({ username });
+        const user = await User.findOne({ user_name });
         if (!user) return res.status(400).send("User not found");
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).send("Invalid credentials");
@@ -28,14 +28,15 @@ exports.login = async (req, res) => {
         const accessToken = jwt.sign(
             { userId: user._id },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "5m" }
+            { expiresIn: "30m" }
         );
 
         const refreshToken = jwt.sign(
             { userId: user._id },
-            process.env.REFRESH_TOKEN_SECRET
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: "45m" } 
         );
-        res.json({ accessToken, refreshToken });
+        res.json({user, accessToken, refreshToken });
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -43,7 +44,7 @@ exports.login = async (req, res) => {
 
 // Refresh
 exports.refresh = async (req, res) => {
-    const { token } = req.body;
+    const token = req.headers['authorization']?.split(' ')[1]; // 'Bearer <token>'
     if (!token) return res.sendStatus(401);
     jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
--- model/user.js
+++ model/user.js
@@ -5,11 +5,12 @@ const userSchema = new mongoose.Schema({
     //ใส่ แอคตีบิว เพื่อสร้างdatabase
     user_name: { type: String, required: true }, //required เป็นการเช็ค ถ้ากำหนด true แสดงว่าจำเป็นต้องใส่ข้อมูลครบ
     password: { type: String, required: true },
-    name: { type: String, required: true },
-    role: { type: String, required: true },
+    name: { type: String, required: false },
+    role: { type: String, required: false },
+    email:{type: String,required:true}
     
 },
 {timestamps: true, versionKey: false} //มันจะใส่วันที่โดยอัติโนมัติ
 );
-const user = mongoose.model('User', userSchema); //ต้องทำการ export และ กำหนดชื่อตัว ที่จะเก็บ productSchema
-module.exports = user
+const user = mongoose.model('users', userSchema,'users'); //ต้องทำการ export และ กำหนดชื่อตัว ที่จะเก็บ productSchema
+module.exports = user;
